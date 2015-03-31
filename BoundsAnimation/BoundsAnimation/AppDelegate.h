795293db7a33b68171b33bf7c249dcd692942c79
Dons (Kim Kwangmin) <k2m719@coupang.com>
2015-03-31 13:38:18 +0900

    소스 추가.

---
+++ BoundsAnimation/BoundsAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BoundsAnimation
+//
+//  Created by Kim Kwangmin on 2/2/15.
+//  Copyright (c) 2015 Kim Kwangmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BoundsAnimation/BoundsAnimation/ViewController.m
@@ -0,0 +1,46 @@
+//
+//  ViewController.m
+//  BoundsAnimation
+//
+//  Created by Kim Kwangmin on 2/2/15.
+//  Copyright (c) 2015 Kim Kwangmin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    UIView *tempView = [[UIView alloc] initWithFrame:CGRectMake(10, 10, 50, 50)];
+    tempView.backgroundColor = [UIColor redColor];
+    [self.view addSubview:tempView];
+    
+    CAKeyframeAnimation *bounceAnimation = [CAKeyframeAnimation animationWithKeyPath:@"transform.scale"];
+    bounceAnimation.values = @[@0.5F, @1.4F, @0.8F, @1.0F];
+    bounceAnimation.duration = 5;
+    bounceAnimation.removedOnCompletion = NO;
+    bounceAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    bounceAnimation.autoreverses = YES;
+    bounceAnimation.delegate = self;
+    
+    [tempView.layer addAnimation:bounceAnimation forKey:@"bounce"];
+}
+
+
+- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)finished
+{
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
